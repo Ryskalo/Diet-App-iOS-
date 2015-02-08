b47215c4b1f3f862971d9fa36c569497c8b6d897
ryskalo <antonryskalev@Antons-MacBook-Pro.local>
2015-02-08 09:23:03 +0300

    ...

--- diet/ViewController.swift
+++ diet/ViewController.swift
@@ -12,18 +12,51 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var weightField: UITextField!
-    
     @IBOutlet weak var heightField: UITextField!
-    
     @IBOutlet weak var oldField: UITextField!
-    
     @IBOutlet weak var caloriesResult: UILabel!
-    
     @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var nameProduct: UITextField!
+    @IBOutlet weak var protProduct: UITextField!
+    @IBOutlet weak var fatProduct: UITextField!
+    @IBOutlet weak var carbProduct: UITextField!
+    @IBOutlet weak var calProduct: UITextField!
+    @IBOutlet weak var status: UILabel!
+    
+    
+    var databasePath = NSString()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let filemgr = NSFileManager.defaultManager()
+        
+        let dirPaths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        
+        println("\(dirPaths)")
+        
+        let docsDir = dirPaths[0] as String
+        
+        databasePath = docsDir.stringByAppendingPathComponent("products.db")
+        
+        if !filemgr.fileExistsAtPath(databasePath) {
+            
+            let productDB = FMDatabase(path: databasePath)
+            
+            
+            if productDB == nil {
+                println("Error: \(productDB.lastErrorMessage())")
+            }
+            
+            if productDB.open() {
+                let sql_stmt = "CREATE TABLE IF NOT EXISTS PRODUCTS (ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, PROT TEXT, FAT TEXT, CARB TEXT, CAL TEXT, IMAGE TEXT)"
+                if !productDB.executeStatements(sql_stmt) {
+                    println("Error: \(productDB.lastErrorMessage())")
+                }
+                productDB.close()
+            } else {
+                println("Error: \(productDB.lastErrorMessage())")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -43,12 +76,12 @@ class ViewController: UIViewController {
         {
         case 0:
             
-          /*  let ruleCalories:Float = (10 * weight + 6.25 * height - 5 * old + 5) * 1.2
+            let ruleCalories:Float = (10 * weight + 6.25 * height - 5 * old + 5) * 1.2
             
             var cal = num.stringFromNumber(ruleCalories)
             
             caloriesResult.text = cal! + " Calories"
-            
+     /*
        //     var data : NSMutableDictionary = NSMutableDictionary(contentsOfFile: path!)!
             
             var object = ["Calor" : "ruleCalories"]
@@ -89,5 +122,31 @@ class ViewController: UIViewController {
             break; 
         }
     }
+    
+    @IBAction func addProductBtn(sender: AnyObject) {
+        
+        let productDB = FMDatabase(path: databasePath)
+        
+        if productDB.open() {
+            
+            let insertSQL = "INSERT INTO PRODUCTS (name, prot, fat, carb, cal, image) VALUES ('\(nameProduct.text)', '\(protProduct.text)', '\(fatProduct.text)', '\(carbProduct.text)', '\(calProduct.text)', '152-512.png')"
+            
+            let result = productDB.executeUpdate(insertSQL,
+                withArgumentsInArray: nil)
+            
+            if !result {
+                status.text = "Failed to add contact"
+                println("Error: \(productDB.lastErrorMessage())")
+                
+                
+            } else {
+                status.text = "Contact Added"
+            }
+        } else {
+            println("Error: \(productDB.lastErrorMessage())")
+        }
+        
+    }
+    
 }
 
